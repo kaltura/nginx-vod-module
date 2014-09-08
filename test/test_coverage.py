0e17b950f9d3f1e8c040c650084bb223a05d0ea0
erankor <eran.kornblau@kaltura.com>
2014-09-08 18:34:36 +0300

    update tests

--- test/test_coverage.py
+++ test/test_coverage.py
@@ -2,7 +2,14 @@ import sys
 import re
 import os
 
-_, nginxVodRoot, errorLog = sys.argv
+if len(sys.argv) < 2:
+    print 'Usage:\n\t%s <nginx-vod code root> [<error log file>]'
+    sys.exit(1)
+
+nginxVodRoot = sys.argv[1]
+errorLog = None
+if len(sys.argv) > 2:
+    errorLog = sys.argv[2]
 
 logMessages = []
 logPrefixes = set([])
@@ -10,8 +17,10 @@ for root, dirs, files in os.walk(nginxVodRoot):
     for name in files:
         if os.path.splitext(name)[1] != '.c':
             continue
-        for curLog in re.findall('(?:ngx|vod)_log_(?:debug|error)[^\(]*\([^\)]+\)', file(os.path.join(root, name), 'rb').read()):
+        fileData = file(os.path.join(root, name), 'rb').read()
+        for curLog in re.findall('(?:ngx|vod)_log_(?:debug|error)[^\(]*\([^\)]+\)', fileData):
             logMessage = curLog.split(',')[3].strip()
+            
             if logMessage.endswith('")'):
                 logMessage = logMessage[:-1]
             if logMessage.endswith('"'):
@@ -22,10 +31,27 @@ for root, dirs, files in os.walk(nginxVodRoot):
             if logMessage == 'const char *fmt':
                 continue
 
+            # add the log message
             logPrefix = logMessage.split('%')[0]
             logMessages.append((logPrefix, logMessage))
             logPrefixes.add(logPrefix)
 
+            # validate the function name in the log line matches the actual function
+            linePos = fileData.find(curLog)
+            functionNames = re.findall('^[a-z0-9_]+\(', fileData[:linePos], re.MULTILINE)
+            realFunction = functionNames[-1]
+            if realFunction.endswith('('):
+                realFunction = realFunction[:-1]
+            logFunction = logMessage.split(':')[0]
+            if logFunction.startswith('"'):
+                logFunction = logFunction[1:]
+            if realFunction != logFunction:
+                print 'log function mismatch %s' % logMessage
+            
+
+if errorLog == None:
+    sys.exit(0)
+            
 errorLogData = file(errorLog, 'rb').read()
 foundPrefixes = {}
 for logPrefix in logPrefixes:
