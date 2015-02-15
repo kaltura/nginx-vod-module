a6c17ed1dffa967dc1e66422e4e3b9fed46157d9
erankor <eran.kornblau@kaltura.com>
2015-02-15 10:48:33 +0200

    compare the response headers in addition to body

--- test/uri_compare.py
+++ test/uri_compare.py
@@ -1,4 +1,5 @@
 from httplib import BadStatusLine
+import email.utils as eut
 import stress_base
 import urlparse
 import hashlib
@@ -10,6 +11,17 @@ import hmac
 import re
 
 from uri_compare_params import *
+
+INGORED_HEADERS = set([
+	'x-vod-me','x-vod-session',
+])
+
+IGNORE_HEADER_VALUES = set([
+	'date',
+	'content-length',		# ignore content length since we compare the buffer, the content length may be different due to different host name lengths
+	'etag',					# derived from content length
+])
+
 
 class TestThread(stress_base.TestThreadBase):
 	def __init__(self, index, increment, stopFile):
@@ -32,22 +44,63 @@ class TestThread(stress_base.TestThreadBase):
 			G2O_SIGN_HEADER_NAME: sign,
 			}
 
+	def parseHttpHeaders(self, headers):
+		result = {}
+		for header in headers:
+			header = map(lambda y: y.strip(), header.split(':', 1))
+			headerName = header[0].lower()
+			headerValue = header[1] if len(header) > 1 else ''
+			result.setdefault(headerName, [])
+			result[headerName].append(headerValue)
+		return result
+		
+	@staticmethod
+	def parseHttpTime(timeStr):
+		return time.mktime(eut.parsedate(timeStr))
+		
+	def compareHeaders(self, headers1, headers2):
+		for headerName in INGORED_HEADERS:
+			headers1.pop(headerName, None)
+			headers2.pop(headerName, None)
+		
+		onlyIn1 = set(headers1.keys()) - set(headers2.keys())
+		if len(onlyIn1) != 0:
+			self.writeOutput('Error: headers %s found only in headers1' % (','.join(onlyIn1)))
+			return False
+		onlyIn2 = set(headers2.keys()) - set(headers1.keys())
+		if len(onlyIn2) != 0 and onlyIn2 != set(['access-control-allow-origin']):		# allow CORS to be added
+			self.writeOutput('Error: headers %s found only in headers2' % (','.join(onlyIn2)))
+			return False
+		for curHeader in headers1.keys():
+			if curHeader in IGNORE_HEADER_VALUES:
+				continue
+			value1 = headers1[curHeader]
+			value2 = headers2[curHeader]
+			if value1 == value2:
+				continue
+			if curHeader == 'expires':
+				if abs(self.parseHttpTime(value1[0]) - self.parseHttpTime(value2[0])) < 10:
+					continue
+			self.writeOutput('Error: different value for header %s - %s vs %s' % (curHeader, value1, value2))
+			return False
+		return True
+
 	def getURL(self, url):
 		request = urllib2.Request(url, headers=self.getG2OHeaders(url))
 		try:
 			f = urllib2.urlopen(request)
-			return f.getcode(), f.read()
+			return f.getcode(), self.parseHttpHeaders(f.info().headers), f.read()
 		except urllib2.HTTPError, e:
-			return e.getcode(), e.read()			
+			return e.getcode(), self.parseHttpHeaders(e.info().headers), e.read()			
 		except urllib2.URLError, e:
 			self.writeOutput('Error: request failed %s %s' % (url, e))
-			return 0, ''
+			return 0, {}, ''
 		except BadStatusLine, e:
 			self.writeOutput('Error: request failed %s %s' % (url, e))
-			return 0, ''
+			return 0, {}, ''
 		except socket.error, e:
 			self.writeOutput('Error: got socket error %s %s' % (url, e))
-			return 0, ''
+			return 0, {}, ''
 		
 	def runTest(self, uri):		
 		url1 = URL1_BASE + uri
@@ -59,11 +112,15 @@ class TestThread(stress_base.TestThreadBase):
 		url1 = re.sub('/p/\d+/sp/\d+/', '/p/%s/sp/%s00/' % (TEST_PARTNER_ID, TEST_PARTNER_ID), url1)
 		url2 = re.sub('/p/\d+/sp/\d+/', '/p/%s/sp/%s00/' % (TEST_PARTNER_ID, TEST_PARTNER_ID), url2)
 
-		code1, body1 = self.getURL(url1)
-		code2, body2 = self.getURL(url2)
+		code1, headers1, body1 = self.getURL(url1)
+		code2, headers2, body2 = self.getURL(url2)
 		if code1 != code2:
 			self.writeOutput('Error: got different status codes %s vs %s' % (code1, code2))
 			return False
+		
+		if not self.compareHeaders(headers1, headers2):
+			self.writeOutput('Error: got different headers %s vs %s' % (headers1, headers2))
+			return False			
 		
 		if str(code1) != '200':
 			self.writeOutput('Notice: got status code %s' % (code1))
