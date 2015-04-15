465fbaa86c581ef1477eb75137e9b5455d2e9ec2
Jess Portnoy <kernel01@gmail.com>
2015-04-15 16:38:47 +0300

    nu1

--- travis_build.sh
+++ travis_build.sh
@@ -36,7 +36,7 @@ unzip -oqq nginx-akamai-token-validate-module-$KALTURA_NGINX_AKAMAI_TOKEN_VALIDA
 #export LD_LIBRARY_PATH LIBRARY_PATH C_INCLUDE_PATH" > ~/.bashrc
 #. ~/.bashrc
 sudo find /opt/kaltura/ffmpeg-2.1.3/lib/ -name "*so" -exec ln -s {} /usr/local/lib \;
-find /opt/kaltura/ffmpeg-2.1.3/include/ -type d -exec ln -s {} /usr/local/include \;
+sudo find /opt/kaltura/ffmpeg-2.1.3/include/ -type d -exec ln -s {} /usr/local/include \;
 
 ./configure \
         --prefix=/etc/nginx \
