b001283e96a5419df48570c3e2b8fabddf502770
Jess Portnoy <jess.portnoy@kaltura.com>
2016-11-28 12:33:25 +0000

    .

--- travis_build.sh
+++ travis_build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 set -o nounset                              # Treat unset variables as an error
 
 NGINX_VERSION=1.10.2
@@ -13,7 +13,7 @@ mkdir -p /tmp/builddir/nginx-$NGINX_VERSION
 cp -r . /tmp/builddir/nginx-$NGINX_VERSION/nginx-vod-module
 cd /tmp/builddir
 wget $NGINX_URI -O kaltura-nginx-$NGINX_VERSION.tar.gz
-tar zxvf kaltura-nginx-$NGINX_VERSION.tar.gz
+tar zxf kaltura-nginx-$NGINX_VERSION.tar.gz
 cd nginx-$NGINX_VERSION
 
 LD_LIBRARY_PATH=/opt/kaltura/ffmpeg-2.1.3/lib
