b61f6da99b79aa87391918e8c94d20af911d9ce4
Travis CI <jess.portnoy@kaltura.com>
2022-06-27 12:54:31 +0100

    New mainline version - 1.23.0

--- ci_build.sh
+++ ci_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -o nounset                              # Treat unset variables as an error
 
-NGINX_VERSION=1.21.2
+NGINX_VERSION=1.23.0
 NGINX_URI="http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz"
 
 
